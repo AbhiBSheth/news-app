ecfa484b149af7600a2e2db6edaeae3d8f3ad5e2
Abhi Sheth <128669457+AbhiBSheth@users.noreply.github.com>
2024-04-03 00:39:28 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,40 @@
+const Navbar = ({setCategory}) => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-4">NewsMag</span></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        
+        <li className="nav-item">
+          <button className="nav-link" onClick={()=>setCategory("technology")} >Technology</button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link" onClick={()=>setCategory("business")} >Business</button>
+        </li>
+        <li className="nav-item" onClick={()=>setCategory("health")}>
+          <button className="nav-link" >Health</button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link" onClick={()=>setCategory("science")}>Science</button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link" onClick={()=>setCategory("sports")}>Sports</button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link" onClick={()=>setCategory("entertainment")}>Entertainment</button>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
